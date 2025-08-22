b244241adea8b067f1cc96ba6c0c83b4611c32f4
Anton Sizov <raindropua@gmail.com>
2025-08-22 12:34:12 +0300

    chore: implement the layout structure with header and footer components, add home page and quiz page templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@core/layout/layout/layout.component';
 
 export const routes: Routes = [
   {
-    path: 'quiz',
-    loadChildren: () =>
-      import('./features/quiz/quiz.routes').then((m) => m.QUIZ_ROUTES),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        data: {
+          preload: true,
+          seo: {},
+        },
+        loadComponent: () =>
+          import('./pages/home/components/home/home-page.component').then(
+            (m) => m.HomePageComponent,
+          ),
+      },
+      {
+        path: 'quiz',
+        loadChildren: () =>
+          import('./features/quiz/quiz.routes').then((m) => m.QUIZ_ROUTES),
+      },
+    ],
   },
   { path: '', redirectTo: 'quiz', pathMatch: 'full' },
 ];
